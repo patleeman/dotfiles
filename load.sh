4c2e0b22a60b712c55bbe584cbeaf34a392bd76e
Patrick Lee <me@patricklee.nyc>
2021-12-20 14:27:29 -0500

    Update load

--- load.sh
+++ load.sh
@@ -2,5 +2,18 @@
 
 DOTFILES_DIR="${HOME}/dotfiles"
 
-for f in $DOTFILES_DIR/configs/*; do source $f; done
-for f in $DOTFILES_DIR/local/*; do source $f; done
+# Set nullglob to prevent error if local directory is empty.
+if [ -n "$ZSH_VERSION" ]; then
+    set -o nullglob
+fi
+
+if [ -n "$BASH_VERSION" ]; then
+    shopt -s nullglob
+fi
+
+# Source the configuration files.
+for f in $DOTFILES_DIR/{configs,local}/*; do
+    if [[ -r $f ]] && [[ -f $f ]]; then
+        source $f
+    fi
+done

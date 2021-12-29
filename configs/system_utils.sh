646048a06db59f54b009975f4bd9ae8eac0cd58d
Patrick Lee <me@patricklee.nyc>
2021-12-29 15:18:40 -0500

    Add autoupdate

--- configs/system_utils.sh
+++ configs/system_utils.sh
@@ -10,9 +10,16 @@ function copy_ssh_key_to_clipboard() {
 
 function update_dotfiles() {
     if (( $(git rev-list HEAD...origin/master --count) > 0 )); then
+        echo "There are dotfiles updates available. Update? [Yn]"
+        read answer
+        if [ "$answer" == "n"]; then
+            echo "To update manually run update_dotfiles"
+            return 1
+        fi
+
         echo "Updating dotfiles"
         git fetch origin master
-        git reset --hard FETCH_HEAD
+        # git reset --hard FETCH_HEAD
         echo "Updating dotfiles complete"
     fi
 }

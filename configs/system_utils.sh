ef9a6750995f07eea4175367ebeac26ed843c99c
Patrick Lee <me@patricklee.nyc>
2021-12-29 15:11:28 -0500

    Add update_dotfiles function

--- configs/system_utils.sh
+++ configs/system_utils.sh
@@ -7,3 +7,12 @@ function copy_ssh_key_to_clipboard() {
         echo "$key" | xclip -selection clipboard
     fi
 }
+
+function update_dotfiles() {
+    if (( $(git rev-list HEAD...origin/master --count) > 0 )); then
+        echo "Updating dotfiles"
+        git fetch origin master
+        git reset --hard FETCH_HEAD
+        echo "Updating dotfiles complete"
+    fi
+}

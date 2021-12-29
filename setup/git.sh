4b94edb9c197c1a432fba7cd9f399be29c9c5162
Patrick Lee <me@patricklee.nyc>
2021-12-29 14:51:24 -0500

    Add pull.rebase true

--- setup/git.sh
+++ setup/git.sh
@@ -2,6 +2,7 @@
 
 # Make Git not complain about pushing upstream branches.
 git config --global push.default current
+git config --global pull.rebase true
 git config --global code.editor "code --wait"
 git config --global diff.tool "vscode"
 git config --global difftool.vscode.cmd 'code --wait --diff $REMOTE $LOCAL'

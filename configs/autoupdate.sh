0ee25c9f976d010c8077682e4c24fe05ff1b5116
Patrick Lee <me@patricklee.nyc>
2022-01-03 21:30:21 -0500

    Fix autoupdate date calculation

--- configs/autoupdate.sh
+++ configs/autoupdate.sh
@@ -22,8 +22,7 @@ function check_for_updates() {
     update_threshold=86400
     last_update_file="$DOTFILES_DIR/.last_update"
     [ ! -e "$last_update_file" ] && touch "$last_update_file"
-    last_check_ts=$(cat "$last_update_file")
-    last_check=$(date -r "$last_check_ts" +%s)
+    last_check=$(cat "$last_update_file")
     time_now=$(date +%s)
     time_since_check=$((time_now - last_check))
     if [[ "$time_since_check" -ge "$update_threshold" ]]; then

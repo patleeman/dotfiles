11be47ba042f6a5ba093e121a0326aa1da1f77b6
Patrick Lee <patrick@Patricks-Air.fios-router.home>
2022-01-06 10:23:46 -0500

    Add global user.name and user.email

--- setup/git.sh
+++ setup/git.sh
@@ -1,5 +1,8 @@
 #!/usr/bin/env sh
 
+git config --global user.name "Patrick Lee"
+git config --global user.email "git@patricklee.nyc"
+
 # Make Git not complain about pushing upstream branches.
 git config --global push.default current
 git config --global pull.rebase true

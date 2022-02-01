ee82f64f6abadb90aa237894feb300a00892c0e2
Patrick Lee <git@patricklee.nyc>
2022-02-01 09:52:44 -0500

    Update lazy nvm

--- configs/lazy_nvm.sh
+++ configs/lazy_nvm.sh
@@ -4,33 +4,22 @@
 # https://gist.github.com/fl0w/07ce79bd44788f647deab307c94d6922
 # all props goes to http://broken-by.me/lazy-load-nvm/
 # grabbed from reddit @ https://www.reddit.com/r/node/comments/4tg5jg/lazy_load_nvm_for_faster_shell_start/
+# Add every binary that requires nvm, npm or node to run to an array of node globals
+NODE_GLOBALS=(`find ~/.nvm/versions/node -maxdepth 3 -type l -wholename '*/bin/*' | xargs -n1 basename | sort | uniq`)
+NODE_GLOBALS+=("node")
+NODE_GLOBALS+=("nvm")
+NODE_GLOBALS+=("npx")
 
-lazynvm() {
-  unset -f nvm node npm npx
+# Lazy-loading nvm + npm on node globals call
+load_nvm () {
   export NVM_DIR=~/.nvm
   [ -s "$NVM_DIR/nvm.sh" ] && . "$NVM_DIR/nvm.sh" # This loads nvm
   if [ -f "$NVM_DIR/bash_completion" ]; then
-    # shellcheck source=/dev/null
     [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion" # This loads nvm bash_completion
   fi
 }
 
-nvm() {
-  lazynvm
-  nvm "$@"
-}
-
-node() {
-  lazynvm
-  node "$@"
-}
-
-npm() {
-  lazynvm
-  npm "$@"
-}
-
-npx() {
-  lazynvm
-  npx "$@"
-}
+# Making node global trigger the lazy loading
+for cmd in "${NODE_GLOBALS[@]}"; do
+  eval "${cmd}(){ unset -f ${cmd} >/dev/null 2>&1; load_nvm; ${cmd} \$@; }"
+done

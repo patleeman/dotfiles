14a2bf11310716c9df0d5b6c7b4185695f68b062
Patrick Lee <git@patricklee.nyc>
2023-03-07 11:35:49 -0500

    Add handy function to jump past brackets

--- home/nvim/lua/core/mappings.lua
+++ home/nvim/lua/core/mappings.lua
@@ -20,6 +20,32 @@ M.general = {
 
     -- jj to escape
     ["jj"] = { "<ESC>", "remap escape to jj" },
+
+    -- Jump outside the first closing character. Useful with auto-bracketing 
+    -- https://old.reddit.com/r/neovim/comments/rztkaq/how_do_you_jump_out_pairs_or_quotes_in_insert/hrzolns/
+    ["ll"] = {
+      function()
+        local closers = {")", "]", "}", ">", "'", '"', "`", ","}
+        local line = vim.api.nvim_get_current_line()
+        local row, col = unpack(vim.api.nvim_win_get_cursor(0))
+        local after = line:sub(col + 1, -1)
+        local closer_col = #after + 1
+        local closer_i = nil
+        for i, closer in ipairs(closers) do
+          local cur_index, _ = after:find(closer)
+          if cur_index and (cur_index < closer_col) then
+            closer_col = cur_index
+            closer_i = i
+          end
+        end
+        if closer_i then
+          vim.api.nvim_win_set_cursor(0, {row, col + closer_col})
+        else
+          vim.api.nvim_win_set_cursor(0, {row, col + 1})
+        end
+      end,
+    "escape pair"
+    },
   },
 
   n = {

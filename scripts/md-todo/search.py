1644b087a9091bd0afecc3aa755e1c6b43a94acd
Patrick Lee <git@patricklee.nyc>
2023-03-16 09:31:40 -0400

    Script working!

--- scripts/md-todo/search.py
+++ scripts/md-todo/search.py
@@ -4,6 +4,10 @@ import subprocess
 from pyfzf.pyfzf import FzfPrompt
 from hashlib import md5
 from enum import StrEnum
+from dataclasses import dataclass
+
+
+NOTES_DIR = "~/Dropbox/Notes"
 
 
 class Actions(StrEnum):
@@ -12,8 +16,24 @@ class Actions(StrEnum):
     OPEN_VIM = "Open file in Neovim"
 
 
+class TodoStates(StrEnum):
+    OPEN = "[ ]"
+    DONE = "[x]"
+    WONT_DO = "[-]"
+
+
+@dataclass
+class TodoItem:
+    path: str
+    raw: str
+    line: int
+    todo: str
+    hash: str
+    short_hash: str
+
+
 def parse_files(directory):
-    checklist_items = []
+    checklist_items: [TodoItem] = []
     for root, dirs, files in os.walk(directory):
         # TODO: Add an ignore file
         for file in files:
@@ -21,38 +41,59 @@ def parse_files(directory):
                 file_path = os.path.join(root, file)
                 with open(file_path, "r") as f:
                     for line_num, line in enumerate(f):
-                        if "[ ]" in line:
+                        if TodoStates.OPEN in line:
                             text = line.strip()
-                            todo_index = text.index("[ ]") + 3
+                            todo_index = text.index(TodoStates.OPEN) + 3
                             todo_item = text[todo_index:].strip()
-                            payload = {
-                                "path": file_path,
-                                "raw": text,
-                                "line": line_num + 1,
-                                "todo": todo_item,
-                                "hash": md5(todo_item.encode('utf8')).hexdigest()
-                            }
+                            hash = md5(todo_item.encode('utf8')).hexdigest()
+                            payload = TodoItem(
+                                file_path,
+                                line,
+                                line_num + 1,
+                                todo_item,
+                                hash,
+                                hash[0:4]
+                            )
                             checklist_items.append(payload)
     return checklist_items
 
 
-if __name__ == "__main__":
-    fzf = FzfPrompt()
-    NOTES_DIR = "~/Dropbox/Notes"
-    notes_path = os.path.expanduser(NOTES_DIR)
-    todo_items = parse_files(notes_path)
-    display = [
-        f"{x['hash'][0:4]}: {x['todo']}" for x in todo_items]
+# TODO: Append metadata like done:2023-01-01 or wontdo:2023-01-01
+def mark_todo(todo: TodoItem, state: TodoStates):
+    with open(todo.path, 'r') as f:
+        lines = f.readlines()
 
-    chosen_item = fzf.prompt(display)
-    if not len(chosen_item) > 0:
+    line_to_modify = lines[todo.line - 1]
+    if not line_to_modify == todo.raw:
+        print("in-file: ", line_to_modify)
+        print("expected: ", todo.raw)
+        print("The file was modified! Not writing")
+        exit(1)
+
+    line_to_modify = line_to_modify.replace(TodoStates.OPEN, state)
+    lines[todo.line - 1] = line_to_modify
+
+    with open(todo.path, 'w') as f:
+        f.writelines(lines)
+
+
+def prompt():
+    fzf = FzfPrompt()
+    notes_path = os.path.expanduser(NOTES_DIR)
+    todo_items: [TodoItem] = parse_files(notes_path)
+
+    fzf_display_strings = [
+        f"{x.short_hash}: {x.todo}" for x in todo_items]
+
+    chosen_todo = fzf.prompt(fzf_display_strings)
+    if not len(chosen_todo) > 0:
         exit(0)
 
-    id = chosen_item[0].split(":")[0]
-    found_todo = None
-    for todo in todo_items:
-        if id in todo["hash"]:
-            found_todo = todo
+    id = chosen_todo[0].split(":")[0]
+    todo_to_update: TodoItem | None = None
+    for todo_item in todo_items:
+        if id == todo_item.short_hash:
+            todo_to_update = todo_item
 
     action = fzf.prompt([Actions.OPEN_VIM, Actions.DONE,
                         Actions.WONT_DO])
@@ -61,11 +102,18 @@ if __name__ == "__main__":
 
     action = action[0]
     if action == Actions.DONE:
-        print('DONE')
+        mark_todo(todo_to_update, TodoStates.DONE)
+        print(
+            f"Line {todo_to_update.line} in {todo_to_update.path} marked as done")
     elif action == Actions.WONT_DO:
-        print("Won't Do")
+        mark_todo(todo_to_update, TodoStates.WONT_DO)
+        print(
+            f"Line {todo_to_update.line} in {todo_to_update.path} marked as won't do")
     elif action == Actions.OPEN_VIM:
-        subprocess.call(['nvim', f'+{found_todo["line"]}', found_todo['path']])
+        subprocess.call(
+            ['nvim', f'+{todo_to_update.line}', todo_to_update.path])
         exit(0)
-    elif action == Actions.EXIT:
-        print("DO NOTHING")
+
+
+if __name__ == "__main__":
+    prompt()

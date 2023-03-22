9c9a5d03a134cd79faac699622424be003b3140a
Patrick Lee <git@patricklee.nyc>
2023-03-21 20:12:37 -0400

    Simplify install script

--- install/osx_arm.zsh
+++ install/osx_arm.zsh
@@ -1,11 +1,10 @@
 #!/usr/bin/env zsh
 
-# Install Applications
-echo "Installing Homebrew"
+# Install homebrew
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 eval "$(/opt/homebrew/bin/brew shellenv)"
 
-echo "Installing oh-my-zsh"
+# Oh-my-zsh
 sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 # Fix shell completion permissions
 # https://github.com/zsh-users/zsh-completions/issues/433#issuecomment-799542335
@@ -14,88 +13,45 @@ chmod -R go-w "$(brew --prefix)/share"
 brew tap homebrew/cask-versions
 brew tap homebrew/cask-fonts
 
-echo "Install thefuck utility"
-brew install thefuck
-
-echo "Installing vscode"
-brew install --cask visual-studio-code
-
-echo "Installing Git Fork"
+brew install \
+  thefuck \
+  tldr \
+  docker \
+  nvm \
+  pyenv \
+  pyenv-virtualenv \
+  direnv \
+  awscli \
+  pandoc \
+  exa \
+  jq \
+  neovim \
+  ripgrep \
+  lazygit \
+  tmux \
+  fzf \
+  shellcheck \
+  koekeishiya/formulae/yabai \
+  iterm2
 brew install --cask fork
+brew install --cask font-fira-code
+brew install --cask font-fira-code-nerd-font
+brew install --cask linearmouse
 
-echo "Installing Firefox"
-brew install --cask firefox
-
-echo "Installing Docker"
-brew install docker
-
-echo "Installing nvm & node"
-brew install nvm
+# Set up node
 nvm install --lts
 nvm use --lts
-# Set default node version for system
 nvm alias default node
 
-npm install -g tldr
-
-echo "Installing pyenv & pyenv-virtualenv"
-brew install pyenv
-brew install pyenv-virtualenv
+# Set up pyenv
 pyenv install 3
 pyenv global 3
 
-echo "Installing direnv"
-brew install direnv
-
-echo "Installing awscli"
-brew install awscli
-
-echo "Installing FiraCode Font"
-brew install --cask font-fira-code
-brew install --cask font-fira-code-nerd-font
-
-echo "Installing Pandoc"
-brew install pandoc
-
-echo "Installing exa"
-brew install exa
-
-echo "Installing jq"
-brew install jq
-
-echo "Installing Neovim"
-brew install neovim
-
-echo "Installing ripgrep"
-brew install ripgrep
-
-echo "Installing lazygit"
-brew install lazygit
-
-echo "Installing tmux"
-brew install tmux
-
-echo "Installing fzf"
-brew install fzf
-
-echo "Installing zsh-vi-mode"
-brew install zsh-vi-mode
-
-echo "Shellcheck"
-brew install shellcheck
-
-
-echo "Installing Yabai"
-brew install brew install koekeishiya/formulae/yabai
-
-echo "Installing Linear Mouse"
-brew install --cask linearmouse
-
-echo "Installing InYourFace"
+# Install InYourFace
 mas install 1476964367
 
-echo "Installing Rust"
+# Install Rust
 curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
 
-echo "Install neovim-remote"
+# https://github.com/mhinz/neovim-remote
 pip3 install neovim-remote

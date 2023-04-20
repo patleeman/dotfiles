e7e38f675c450f1249e46a1a74575fe37d68316b
Patrick Lee <git@patricklee.nyc>
2023-04-20 12:38:11 -0400

    Tweaks

--- home/nvim/lua/config/keymaps.lua
+++ home/nvim/lua/config/keymaps.lua
@@ -3,27 +3,27 @@
 -- Add any additional keymaps here
 
 -- INSERT MODE KEYMAPS
-vim.keymap.set("i", "jj", "<ESC>", { desc = "Escape insert mode" })
-vim.keymap.set("i", ";;", function()
-  local closers = { ")", "]", "}", ">", "'", '"', "`", "," }
-  local line = vim.api.nvim_get_current_line()
-  local row, col = unpack(vim.api.nvim_win_get_cursor(0))
-  local after = line:sub(col + 1, -1)
-  local closer_col = #after + 1
-  local closer_i = nil
-  for i, closer in ipairs(closers) do
-    local cur_index, _ = after:find(closer)
-    if cur_index and (cur_index < closer_col) then
-      closer_col = cur_index
-      closer_i = i
-    end
-  end
-  if closer_i then
-    vim.api.nvim_win_set_cursor(0, { row, col + closer_col })
-  else
-    vim.api.nvim_win_set_cursor(0, { row, col + 1 })
-  end
-end, { desc = "Escape a pair" })
+-- vim.keymap.set("i", "jj", "<ESC>", { desc = "Escape insert mode" })
+-- vim.keymap.set("i", ";;", function()
+--   local closers = { ")", "]", "}", ">", "'", '"', "`", "," }
+--   local line = vim.api.nvim_get_current_line()
+--   local row, col = unpack(vim.api.nvim_win_get_cursor(0))
+--   local after = line:sub(col + 1, -1)
+--   local closer_col = #after + 1
+--   local closer_i = nil
+--   for i, closer in ipairs(closers) do
+--     local cur_index, _ = after:find(closer)
+--     if cur_index and (cur_index < closer_col) then
+--       closer_col = cur_index
+--       closer_i = i
+--     end
+--   end
+--   if closer_i then
+--     vim.api.nvim_win_set_cursor(0, { row, col + closer_col })
+--   else
+--     vim.api.nvim_win_set_cursor(0, { row, col + 1 })
+--   end
+-- end, { desc = "Escape a pair" })
 
 -- NORMAL MODE KEYMAPS
 vim.keymap.set("n", "<C-d>", "<C-d>zz", { desc = "Jump down and center" })

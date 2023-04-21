58aa740b28f6fd973f2476aeeeef1b52aee62286
Patrick Lee <git@patricklee.nyc>
2023-04-21 11:16:06 -0400

    Remove chat gpt plugin

--- home/nvim/lua/plugins/core.lua
+++ home/nvim/lua/plugins/core.lua
@@ -48,16 +48,4 @@ return {
       },
     },
   },
-  {
-    "jackMort/ChatGPT.nvim",
-    event = "VeryLazy",
-    config = function()
-      require("chatgpt").setup()
-    end,
-    dependencies = {
-      "MunifTanjim/nui.nvim",
-      "nvim-lua/plenary.nvim",
-      "nvim-telescope/telescope.nvim",
-    },
-  },
 }

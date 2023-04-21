8065984cedcc229948638ba1145e4fbb5c3f1f88
Patrick Lee <git@patricklee.nyc>
2023-04-21 10:21:23 -0400

    Add ChatGPT api Key

--- home/nvim/lua/plugins/core.lua
+++ home/nvim/lua/plugins/core.lua
@@ -48,4 +48,16 @@ return {
       },
     },
   },
+  {
+    "jackMort/ChatGPT.nvim",
+    event = "VeryLazy",
+    config = function()
+      require("chatgpt").setup()
+    end,
+    dependencies = {
+      "MunifTanjim/nui.nvim",
+      "nvim-lua/plenary.nvim",
+      "nvim-telescope/telescope.nvim",
+    },
+  },
 }

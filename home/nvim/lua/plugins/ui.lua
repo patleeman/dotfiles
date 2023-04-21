8c326aca9a1b7f6aaa6bafc4a23e50c00e20e523
Patrick Lee <git@patricklee.nyc>
2023-04-21 09:24:20 -0400

    Customize lualine, remove time

--- home/nvim/lua/plugins/ui.lua
+++ home/nvim/lua/plugins/ui.lua
@@ -53,4 +53,17 @@ return {
       top_down = false,
     },
   },
+  {
+    "nvim-lualine/lualine.nvim",
+    opts = {
+      sections = {
+        lualine_y = {
+          { "progress", separator = " ", padding = { left = 1, right = 1 } },
+        },
+        lualine_z = {
+          { "location", padding = { left = 1, right = 1 } },
+        },
+      },
+    },
+  },
 }

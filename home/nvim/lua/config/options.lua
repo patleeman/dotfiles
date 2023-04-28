e5911e225151acaff673c2ceff06a4ff81552794
Patrick Lee <git@patricklee.nyc>
2023-04-28 15:11:40 -0400

    Disable popup menu warning

--- home/nvim/lua/config/options.lua
+++ home/nvim/lua/config/options.lua
@@ -1,7 +1,14 @@
 -- Options are automatically loaded before lazy.nvim startup
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
---
 
+-- Don't hide any text
 vim.opt.conceallevel = 0
+
+-- Don't replace whitespace with >+- chars
 vim.opt.list = false
+
+-- Disable the "How to disable mouse" warning in the right click menu
+vim.api.nvim_command("aunmenu PopUp.How-to\\ disable\\ mouse")
+
+vim.api.nvim_command("aunmenu PopUp.-1-")

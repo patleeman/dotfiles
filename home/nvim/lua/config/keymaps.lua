1a3c8eef277cea732e1e8b54870415e7e3c6cbb5
Patrick Lee <git@patricklee.nyc>
2023-05-05 08:47:40 -0400

    Move usercmd back into keymaps to ensure command is run

--- home/nvim/lua/config/keymaps.lua
+++ home/nvim/lua/config/keymaps.lua
@@ -15,6 +15,44 @@ vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv", { desc = "Move line down" })
 vim.keymap.set("v", ">", ">gv", { desc = "Move block right" })
 vim.keymap.set("v", ">", ">gv", { desc = "Move block left" })
 
+-- Utility to get listed buffers
+local function get_listed_buffers()
+  local buffers = {}
+  local len = 0
+  for buffer = 1, vim.fn.bufnr("$") do
+    if vim.fn.buflisted(buffer) == 1 then
+      len = len + 1
+      buffers[len] = buffer
+    end
+  end
+
+  return buffers
+end
+
+-- Utility to clear no name buffers
+local function clearNoNameBuffers()
+  local buffers = get_listed_buffers()
+  for _, bufnr in ipairs(buffers) do
+    local ft = vim.api.nvim_buf_get_option(bufnr, "filetype")
+    local name = vim.api.nvim_buf_get_name(bufnr)
+    if ft == "" and name == "" then
+      vim.api.nvim_buf_delete(bufnr, {})
+    end
+  end
+end
+
+-- Command to delete all buffers
+vim.api.nvim_create_user_command("BDeleteAll", function()
+  local buffers = get_listed_buffers()
+  for _, bufnr in ipairs(buffers) do
+    local ft = vim.api.nvim_buf_get_option(bufnr, "filetype")
+    local name = vim.api.nvim_buf_get_name(bufnr)
+    if ft ~= "Alpha" and name ~= "" then
+      vim.api.nvim_buf_delete(bufnr, {})
+    end
+  end
+end, { desc = "Delete all buffers" })
+
 -- Buffers
 vim.keymap.set("n", "<leader>bw", "<cmd>BDeleteAll<CR>", { desc = "Close all unsaved buffers" })
 vim.keymap.set("n", "<leader>bn", "<cmd>enew<CR>", { desc = "Create a new buffer" })

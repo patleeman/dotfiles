5653af7a0bd86f396d3406bb1f317842e90fe376
Patrick Lee <git@patricklee.nyc>
2023-05-30 11:24:35 -0400

    Add script to install work related things

---
+++ install/work.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env sh
+# This file contains any work related configurations I don't use on my other computers
+
+# Java
+brew install openjdk@17 maven gradle
+sudo ln -sfn /opt/homebrew/opt/openjdk@17/libexec/openjdk.jdk /Library/Java/JavaVirtualMachines/openjdk-17.jdk

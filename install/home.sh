311adca0c24edcb3d986bab64157cbbf4c9cc207
Patrick Lee <git@patricklee.nyc>
2023-05-31 09:25:05 -0400

    Move manual items to install

--- install/home.sh
+++ install/home.sh
@@ -1,6 +1,5 @@
 #!/usr/bin/env sh
 
-ln -s ~/dotfiles/home/.todo.cfg ~/.todo.cfg
 ln -s ~/dotfiles/home/.tmux.conf ~/.tmux.conf
 ln -s ~/dotfiles/home/nvim ~/.config/
 ln -s ~/dotfiles/home/yabai ~/.config/

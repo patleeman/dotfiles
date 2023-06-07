b36d5d98c4de384b58c5fcfc94e61d3f7eac5256
Patrick Lee <git@patricklee.nyc>
2023-06-07 08:14:32 -0400

    Add vscode-neovim config

--- home/nvim/init.lua
+++ home/nvim/init.lua
@@ -1,2 +1,6 @@
--- bootstrap lazy.nvim, LazyVim and your plugins
-require("config.lazy")
+if vim.g.vscode then
+    require("vscode.keymaps")
+else
+    -- bootstrap lazy.nvim, LazyVim and your plugins
+    require("config.lazy")
+end

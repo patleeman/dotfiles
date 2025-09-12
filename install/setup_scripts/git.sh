4caccdc4bc90f706ae98a2af5f815d008aa6ddd5
Patrick Lee <me@patricklee.nyc>
2025-09-12 15:14:22 -0400

    Update git.sh

--- install/setup_scripts/git.sh
+++ install/setup_scripts/git.sh
@@ -1,15 +1,3 @@
 #!/usr/bin/env sh
 
-git config --global user.name "Patrick Lee"
-git config --global user.email "git@patricklee.nyc"
-
-# Make Git not complain about pushing upstream branches.
-git config --global push.default current
-git config --global pull.rebase true
-git config --global code.editor "nvim"
-
-# Conform Git branch names to main
-git config --global init.defaultBranch main
-
-# Auto prune
-git config remote.origin.prune true
+git config --global include.path ~/dotfiles/home/.gitconfig
